00a7ba5d79b7f0a71cebda334aa71a0e5c39cef6
JessiLucian <jessforero.2017@gmail.com>
2021-01-26 02:07:22 -0500

    cualquier cosa
    Clase Main funcional

--- src/MainServer.java
+++ src/MainServer.java
@@ -7,41 +7,23 @@ import java.net.Socket;
 
 public class MainServer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		try {
-			
-			ServerSocket ss = new ServerSocket(3333);  
-			
-			Socket s = ss.accept();  				
-		
-			DataInputStream din=new DataInputStream(s.getInputStream());  
-			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  				
-			
-			String str="",str2="";  
-			while(!str.equals("stop")){  
-			str=din.readUTF();  
-			System.out.println("client says: "+str);  
-			str2=br.readLine();  
-			dout.writeUTF(str2);  
-			dout.flush();  
-			}  
-			din.close();  
-			s.close();  
-			ss.close();  
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e);
-			System.out.println("exception here");
+	public static void main(String args[])throws Exception{  
+		ServerSocket ss=new ServerSocket(3333);  
+		Socket s=ss.accept();  
+		DataInputStream din=new DataInputStream(s.getInputStream());  
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+		  
+		String str="",str2="";  
+		while(!str.equals("stop")){  
+		str=din.readUTF();  
+		System.out.println("client says: "+str);  
+		str2=br.readLine();  
+		dout.writeUTF(str2);  
+		dout.flush();  
+		}  
+		din.close();  
+		s.close();  
+		ss.close();  
 		}
-		
-		finally {
-			// TODO: handle finally clause
-			System.out.println("Finally");
-		}
-	}
-
 }

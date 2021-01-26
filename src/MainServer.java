a62052c2d7dc84e54cea03f740bfdc7425fd1016
juansu4rez <jnssuarez@gmail.com>
2021-01-26 23:03:35 +1300

    commit proyecto Server 6

--- src/MainServer.java
+++ src/MainServer.java
@@ -5,38 +5,60 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class MainServer {
 
-	public static void main(String args[]) throws Exception{  
-			  listenToClient();
+public class MainServer {
+	
+	static Socket socket;  
+	static DataInputStream dataInputStream;  
+	static DataOutputStream dataOutputStream;
+
+	public static void main(String args[]){  
+		System.out.println("INICIANDO SERVER");
+		Runnable runnable = () -> listenToClient();
+		Thread thread = new Thread(runnable);
+		thread.start();
+		
+		listenToClient();			 
+			  
 		}
 	
 	private static void listenToClient () {
 		try {
-			ServerSocket ss=new ServerSocket(3333);  
-			Socket s=ss.accept();  
-			DataInputStream din=new DataInputStream(s.getInputStream());  
-			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-			  
-			String str="",str2="";  
+			ServerSocket serverSocket = new ServerSocket(3333);  
+			socket = serverSocket.accept();  
+			dataInputStream = new DataInputStream(socket.getInputStream());  
+			dataOutputStream = new DataOutputStream(socket.getOutputStream());  
+			//BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));  
+			 
+			
+			dataInputStream.close();  
+			socket.close();  
+			serverSocket.close();
+			
+			/*
+			String str = "", str2 = "";  
 			while(!str.equals("stop")){  
-			str=din.readUTF();  
-			System.out.println("client says: "+str);  
-			str2=br.readLine();  
-			dout.writeUTF(str2);  
-			dout.flush();  
+				str = dataInputStream.readUTF();  
+				System.out.println("client says: " + str);  
+				//str2 = bufferedReader.readLine();  
+				dataOutputStream.writeUTF(str2);  
+				dataOutputStream.flush();  
 			}  
-			din.close();  
-			s.close();  
-			ss.close();
+			*/
+			while(true) {
+				System.out.println("Ejecuntado....");
+			}
+			
+			//dataInputStream.close();  
+			//socket.close();  
+			//serverSocket.close();
 		} catch (Exception e) {
 			System.out.println("e : " + e.getMessage());
+			System.out.println(e.getStackTrace());
+			System.out.println(e.getLocalizedMessage());
 		} finally {
-			
+			System.out.println("FIN ");
 		}
 	}
-	
-	
 	
 }

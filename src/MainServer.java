2a87be6a291cd3e72e86576e1bccdaf4a23c8807
juansu4rez <jnssuarez@gmail.com>
2021-01-26 20:15:59 +1300

    commit proyecto Server 4

--- src/MainServer.java
+++ src/MainServer.java
@@ -7,23 +7,36 @@ import java.net.Socket;
 
 public class MainServer {
 
-	public static void main(String args[])throws Exception{  
-		ServerSocket ss=new ServerSocket(3333);  
-		Socket s=ss.accept();  
-		DataInputStream din=new DataInputStream(s.getInputStream());  
-		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-		  
-		String str="",str2="";  
-		while(!str.equals("stop")){  
-		str=din.readUTF();  
-		System.out.println("client says: "+str);  
-		str2=br.readLine();  
-		dout.writeUTF(str2);  
-		dout.flush();  
-		}  
-		din.close();  
-		s.close();  
-		ss.close();  
+	public static void main(String args[]) throws Exception{  
+			  listenToClient();
 		}
+	
+	private static void listenToClient () {
+		try {
+			ServerSocket ss=new ServerSocket(3333);  
+			Socket s=ss.accept();  
+			DataInputStream din=new DataInputStream(s.getInputStream());  
+			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+			  
+			String str="",str2="";  
+			while(!str.equals("stop")){  
+			str=din.readUTF();  
+			System.out.println("client says: "+str);  
+			str2=br.readLine();  
+			dout.writeUTF(str2);  
+			dout.flush();  
+			}  
+			din.close();  
+			s.close();  
+			ss.close();
+		} catch (Exception e) {
+			System.out.println("e : " + e.getMessage());
+		} finally {
+			
+		}
+	}
+	
+	
+	
 }
